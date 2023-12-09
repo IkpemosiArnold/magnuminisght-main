5e71fe47574ff53b85211a22c660d49f9a1e7dbe
Ikpemosi Arnold <Ikpemosi@protonmail.com>
2023-12-09 17:45:50 +0100

    fixed minor error

--- src/app/dashboard/applications/expressentry/page.js
+++ src/app/dashboard/applications/expressentry/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import FormInputGroup from "../FormInputGroup";
